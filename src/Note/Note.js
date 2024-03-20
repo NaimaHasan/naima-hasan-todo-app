a06764f8e8ae0726e347ae23166d04d4d99802c7
NaimaHasan <naimahasan31@gmail.com>
2024-03-20 11:38:55 +0600

    Added empty note display

--- src/Note/Note.js
+++ src/Note/Note.js
@@ -4,20 +4,34 @@ import { NoteCard } from "./NoteCard";
 export const Note = ({ notes, updateNote, deleteNote }) => {
   return (
     <div>
-      <div className="row">
-        {notes.map((note, index) => (
-          <div key={index} className="col mb-4">
-            <NoteCard
-              note={note}
-              updateNote={updateNote}
-              deleteNote={deleteNote}
-            />
-          </div>
-        ))}
-        {notes.length < 4 && (
-          <div className={`col-${12 - notes.length * 3} mb-4`} />
-        )}
-      </div>
+      {notes.length === 0 ? (
+        <div
+          style={{
+            fontSize: "25px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "70vh",
+          }}
+        >
+          <p>Note is currently empty!</p>
+        </div>
+      ) : (
+        <div className="row">
+          {notes.map((note, index) => (
+            <div key={index} className="col mb-4">
+              <NoteCard
+                note={note}
+                updateNote={updateNote}
+                deleteNote={deleteNote}
+              />
+            </div>
+          ))}
+          {notes.length < 4 && (
+            <div className={`col-${12 - notes.length * 3} mb-4`} />
+          )}
+        </div>
+      )}
     </div>
   );
 };
